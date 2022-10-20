38d396f15785ad9a5dd97e6ffcfebe4d53aaddbe
Jason Lenoble <20752619+jlenoble@users.noreply.github.com>
2022-10-20 12:41:57 +0200

    ✨ feat(traits): data trait

---
+++ packages/organon/src/traits/data.rs
@@ -0,0 +1,40 @@
+pub trait IsData {}
+
+pub trait HasData {
+    type Data: IsData;
+
+    fn data(&self) -> &Self::Data;
+}
+
+#[cfg(test)]
+pub mod tests {
+    extern crate alloc;
+    use alloc::{ borrow::ToOwned, string::String };
+    use super::{ HasData, IsData };
+
+    #[derive(Debug, Clone, PartialEq, Eq)]
+    pub struct ItemData(String);
+
+    impl IsData for ItemData {}
+
+    #[test]
+    fn test_has_data() {
+        struct Item {
+            data: ItemData,
+        }
+
+        impl HasData for Item {
+            type Data = ItemData;
+
+            fn data(&self) -> &ItemData {
+                &self.data
+            }
+        }
+
+        let data = Item { data: ItemData("12".to_owned()) };
+
+        assert_eq!(data.data, data.data);
+        assert_eq!(&data.data, data.data());
+        assert_eq!(data.data(), data.data());
+    }
+}
