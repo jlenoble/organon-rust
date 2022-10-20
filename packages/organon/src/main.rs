b9cd5a1963d4f475f2cf0602005dc96b489f740d
Jason Lenoble <20752619+jlenoble@users.noreply.github.com>
2022-10-20 11:50:44 +0200

    ✨ feat(organon): clear main

--- packages/organon/src/main.rs
+++ packages/organon/src/main.rs
@@ -1,15 +1,1 @@
-#![no_std]
-
-use extern_deps::{ std::println };
-use organon::todolist::TodoList;
-
-fn main() {
-    let mut todolist = TodoList::new();
-
-    todolist.create("todo0");
-    todolist.create("todo1");
-    todolist.create("todo2");
-    todolist.create("todo3");
-
-    println!("{}", todolist);
-}
+fn main() {}
