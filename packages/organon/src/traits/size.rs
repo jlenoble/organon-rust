26507f5172541f710964739becd93336b79b3eb6
Jason Lenoble <20752619+jlenoble@users.noreply.github.com>
2022-10-26 16:26:31 +0200

    ✨ feat(organon): addition for size
    Add requires the Sized trait, but for an intuitive usage, we also add
    the Copy trait (to circumvene ubiquitous already borrowed errors).

--- packages/organon/src/traits/size.rs
+++ packages/organon/src/traits/size.rs
@@ -1,13 +1,24 @@
-pub trait IsSize: PartialEq {}
+use core::ops::Add;
+
+pub trait IsSize: Copy + PartialEq + Add {}
 
 #[cfg(test)]
 pub mod tests {
+    use core::ops::Add;
     use super::IsSize;
 
-    #[derive(Debug, PartialEq)]
+    #[derive(Debug, Clone, Copy, PartialEq)]
     struct Size(u16);
 
     impl IsSize for Size {}
+
+    impl Add for Size {
+        type Output = Size;
+
+        fn add(self, s: Size) -> Size {
+            Size(self.0 + s.0)
+        }
+    }
 
     #[test]
     fn size() {
@@ -19,5 +30,20 @@ pub mod tests {
         assert_eq!(s2, s2);
         assert_eq!(s3, s3);
         assert_eq!(s1, s3);
+    }
+
+    #[test]
+    fn add() {
+        let s1 = Size(12);
+        let s2 = Size(4);
+        let s3 = Size(12);
+        let s4 = Size(8);
+
+        assert_eq!(s1 + s1, s1 + s1);
+        assert_eq!(s1 + s2, s1 + s2);
+        assert_eq!(s1 + s2, s2 + s1);
+        assert_eq!(s2 + s4, s1);
+        assert_eq!(s2 + s4, s3);
+        assert_eq!(s1 + s2 + s3 + s4, s3 + s3 + s3);
     }
 }
