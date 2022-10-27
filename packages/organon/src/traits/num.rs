eed0350e62fae8296c0810924d336de790909096
Jason Lenoble <20752619+jlenoble@users.noreply.github.com>
2022-10-27 07:29:36 +0200

    ♻️ refactor(organon): rename Size into Num

--- packages/organon/src/traits/num.rs
+++ packages/organon/src/traits/num.rs
@@ -8,31 +8,31 @@ pub mod tests {
     use super::IsNum;
 
     #[derive(Debug, Clone, Copy, PartialEq)]
-    struct Size(u16);
+    struct Num(u16);
 
-    impl IsNum for Size {}
+    impl IsNum for Num {}
 
-    impl Add for Size {
-        type Output = Size;
+    impl Add for Num {
+        type Output = Num;
 
-        fn add(self, s: Size) -> Size {
-            Size(self.0 + s.0)
+        fn add(self, s: Num) -> Num {
+            Num(self.0 + s.0)
         }
     }
 
-    impl Sub for Size {
-        type Output = Size;
+    impl Sub for Num {
+        type Output = Num;
 
-        fn sub(self, s: Size) -> Size {
-            Size(self.0 - s.0)
+        fn sub(self, s: Num) -> Num {
+            Num(self.0 - s.0)
         }
     }
 
     #[test]
     fn size() {
-        let s1 = Size(12);
-        let s2 = Size(4);
-        let s3 = Size(12);
+        let s1 = Num(12);
+        let s2 = Num(4);
+        let s3 = Num(12);
 
         assert_eq!(s1, s1);
         assert_eq!(s2, s2);
@@ -42,10 +42,10 @@ pub mod tests {
 
     #[test]
     fn add() {
-        let s1 = Size(12);
-        let s2 = Size(4);
-        let s3 = Size(12);
-        let s4 = Size(8);
+        let s1 = Num(12);
+        let s2 = Num(4);
+        let s3 = Num(12);
+        let s4 = Num(8);
 
         assert_eq!(s1 + s1, s1 + s1);
         assert_eq!(s1 + s2, s1 + s2);
@@ -57,15 +57,15 @@ pub mod tests {
 
     #[test]
     fn sub() {
-        let s1 = Size(12);
-        let s2 = Size(4);
-        let s3 = Size(12);
-        let s4 = Size(8);
+        let s1 = Num(12);
+        let s2 = Num(4);
+        let s3 = Num(12);
+        let s4 = Num(8);
 
         assert_eq!(s1 - s1, s1 - s1);
-        assert_eq!(s1 - s1, Size(0));
+        assert_eq!(s1 - s1, Num(0));
         assert_eq!(s1 - s2, s1 - s2);
-        assert_eq!(s1 - s3, Size(0));
+        assert_eq!(s1 - s3, Num(0));
         assert_eq!(s1 - s2, s4);
         assert_eq!(s4 - s2, s2);
         assert_eq!(s1 - s2 - s4, s3 - s2 - s2 - s2);
@@ -73,10 +73,10 @@ pub mod tests {
 
     #[test]
     fn add_sub() {
-        let s1 = Size(12);
-        let s2 = Size(4);
-        let s3 = Size(12);
-        let s4 = Size(8);
+        let s1 = Num(12);
+        let s2 = Num(4);
+        let s3 = Num(12);
+        let s4 = Num(8);
 
         assert_eq!(s1 + s2 - s4, s3 - s2 + s2 - s2 - s4 + s1 - s2);
     }
@@ -84,9 +84,9 @@ pub mod tests {
     #[test]
     #[should_panic]
     fn no_negative_safeguard() {
-        let s1 = Size(12);
-        let s2 = Size(4);
+        let s1 = Num(12);
+        let s2 = Num(4);
 
-        assert_eq!(s2 - s1, Size(0));
+        assert_eq!(s2 - s1, Num(0));
     }
 }
