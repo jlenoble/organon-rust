a2ca07ba9a733a1da7fa8fb4fe12fcae83e28d9c
Jason Lenoble <20752619+jlenoble@users.noreply.github.com>
2022-10-28 17:55:36 +0200

    ✨ feat(derive_builder): step 2
    Passed tests/02-create-builder.rs.

--- packages/derive_builder/src/lib.rs
+++ packages/derive_builder/src/lib.rs
@@ -6,7 +6,26 @@ use syn::{ DeriveInput, parse_macro_input };
 pub fn derive(input: TokenStream) -> TokenStream {
     let _input = parse_macro_input!(input as DeriveInput);
 
-    let expanded = quote! {};
+    let expanded =
+        quote! {
+            pub struct CommandBuilder {
+                executable: Option<String>,
+                args: Option<Vec<String>>,
+                env: Option<Vec<String>>,
+                current_dir: Option<String>,
+            }
+
+            impl Command {
+                pub fn builder() -> CommandBuilder {
+                    CommandBuilder {
+                        executable: None,
+                        args: None,
+                        env: None,
+                        current_dir: None,
+                    }
+                }
+            }
+        };
 
     TokenStream::from(expanded)
 }
