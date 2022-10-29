7b789a4388050c9b90ab0078abff7ebb3260b813
Jason Lenoble <20752619+jlenoble@users.noreply.github.com>
2022-10-29 11:41:47 +0200

    ✨ feat(derive_builder): step 6
    Passed tests/06-optional-field.rs.

--- packages/derive_builder/src/lib.rs
+++ packages/derive_builder/src/lib.rs
@@ -1,10 +1,22 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{ DeriveInput, parse_macro_input };
+use syn::{
+    DeriveInput,
+    parse_macro_input,
+    Data,
+    DataStruct,
+    Fields,
+    FieldsNamed,
+    Type,
+    TypePath,
+    Field,
+    Path,
+    PathSegment,
+};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _input = parse_macro_input!(input as DeriveInput);
+    let input = parse_macro_input!(input as DeriveInput);
 
     // pub struct Command {
     //     executable: String,
@@ -12,6 +24,94 @@ pub fn derive(input: TokenStream) -> TokenStream {
     //     env: Vec<String>,
     //     current_dir: String,
     // }
+
+    let data = match input.data {
+        Data::Struct(data) => data,
+        _ => panic!("Build can only be derived for Structs, not Enums nor Unions"),
+    };
+
+    let fields = match data {
+        DataStruct { fields: Fields::Named(FieldsNamed { named, .. }), .. } => named,
+        _ => panic!("Fields in Struct must be named"),
+    };
+
+    let each_required_field = fields
+        .clone()
+        .into_iter()
+        .filter_map(|field| {
+            match field {
+                Field {
+                    ident,
+                    ty: Type::Path(TypePath { path: Path { segments, .. }, qself: None }),
+                    ..
+                } => {
+                    let name = match ident {
+                        Some(name) => name,
+                        None => panic!("Found unnamed field"),
+                    };
+
+                    let is_option = if let Some(&PathSegment { ref ident, .. }) = segments.first() {
+                        ident == "Option"
+                    } else {
+                        false
+                    };
+
+                    if !is_option {
+                        Some(
+                            quote! { 
+                                #name : match &self.#name {
+                                    Some(#name) => { #name.clone() }
+                                    None => {
+                                        return Err(eyre!("setter was never called on your builder"));
+                                    }
+                                } 
+                            }
+                        )
+                    } else {
+                        None
+                    }
+                }
+                _ => panic!("Expected only named fields"),
+            }
+        });
+
+    let each_optional_field = fields
+        .clone()
+        .into_iter()
+        .filter_map(|field| {
+            match field {
+                Field {
+                    ident,
+                    ty: Type::Path(TypePath { path: Path { segments, .. }, qself: None }),
+                    ..
+                } => {
+                    let name = match ident {
+                        Some(name) => name,
+                        None => panic!("Found unnamed field"),
+                    };
+
+                    let is_option = if let Some(&PathSegment { ref ident, .. }) = segments.first() {
+                        ident == "Option"
+                    } else {
+                        false
+                    };
+
+                    if is_option {
+                        Some(
+                            quote! { 
+                            #name : match &self.#name {
+                                Some(n) => { Some(n.clone()) }
+                                None => None
+                            } 
+                        }
+                        )
+                    } else {
+                        None
+                    }
+                }
+                _ => panic!("Expected only named fields"),
+            }
+        });
 
     let expanded =
         quote! {
@@ -46,35 +146,10 @@ pub fn derive(input: TokenStream) -> TokenStream {
         }
 
         pub fn build(&mut self) -> Result<Command> {
-            let executable = match &self.executable {
-                Some(executable) => { executable.clone() }
-                None => {
-                    return Err(eyre!(".executable() setter was never called on your builder"));
-                }
-            };
-
-            let args = match &self.args {
-                Some(args) => { args.clone() }
-                None => {
-                    return Err(eyre!(".args() setter was never called on your builder"));
-                }
-            };
-
-            let env = match &self.env {
-                Some(env) => { env.clone() }
-                None => {
-                    return Err(eyre!(".env() setter was never called on your builder"));
-                }
-            };
-
-            let current_dir = match &self.current_dir {
-                Some(current_dir) => { current_dir.clone() }
-                None => {
-                    return Err(eyre!(".current_dir() setter was never called on your builder"));
-                }
-            };
-
-            Ok(Command { executable, args, env, current_dir })
+            Ok(Command { 
+                #(#each_required_field ,)*
+                #(#each_optional_field ,)*
+            })
         }
     }
 
