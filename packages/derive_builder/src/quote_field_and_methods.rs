dda0cb0195177bfddf53b8da4903b00efb1bb69c
Jason Lenoble <20752619+jlenoble@users.noreply.github.com>
2022-10-31 21:32:12 +0100

    ♻️ refactor(derive_builder): further cleanup
    Now completely handling method quoting separately.

--- packages/derive_builder/src/quote_field_and_methods.rs
+++ packages/derive_builder/src/quote_field_and_methods.rs
@@ -1,15 +1,11 @@
 use color_eyre::{ Result, eyre::WrapErr };
 use proc_macro2::TokenStream;
-use quote::{ quote, format_ident };
+use quote::format_ident;
 use syn::{ Field, Meta, MetaList, NestedMeta, Lit };
 
 use crate::{
     field_as_named_field_with_attributes_and_type::field_as_named_field_with_attributes_and_type,
-    quote_methods::{
-        quote_all_at_once_setter_method,
-        quote_one_at_a_time_setter_method,
-        quote_both_setter_methods,
-    },
+    quote_methods::quote_methods,
     quote_field::quote_field,
 };
 
@@ -20,7 +16,7 @@ pub fn quote_field_and_methods(field: &Field) -> Result<(TokenStream, TokenStrea
 
     let (is_optional, is_vec) = (type_name == "Option", type_name == "Vec");
 
-    let inert_attr = if !attrs.is_empty() {
+    let method_name = if !attrs.is_empty() {
         if let Some(meta) = attrs.first() {
             if let Ok(Meta::List(MetaList { ref path, ref nested, .. })) = meta.parse_meta() {
                 if path.is_ident("builder") {
@@ -31,8 +27,7 @@ pub fn quote_field_and_methods(field: &Field) -> Result<(TokenStream, TokenStrea
                         if meta.path.is_ident("each") {
                             if let Lit::Str(lit) = &meta.lit {
                                 let lit = format_ident!("{}", lit.value());
-                                let lit2 = lit.clone();
-                                Some((lit, *field_name == lit2))
+                                Some(lit)
                             } else {
                                 None
                             }
@@ -55,21 +50,14 @@ pub fn quote_field_and_methods(field: &Field) -> Result<(TokenStream, TokenStrea
         None
     };
 
-    let quote_field = quote_field(field_name, is_optional);
-
-    let quote_methods = if let Some((ref inert_name, conflicting_method_names)) = inert_attr {
-        if conflicting_method_names {
-            quote_one_at_a_time_setter_method(field_name, inert_name)
-        } else {
-            quote_both_setter_methods(field_name, inert_name)
-        }
+    let method_name = if let Some(ref method_name) = method_name {
+        Some(method_name)
     } else {
-        quote_all_at_once_setter_method(field_name, if is_vec {
-            quote! { Vec<String> }
-        } else {
-            quote! { String }
-        })
+        None
     };
+
+    let quote_field = quote_field(field_name, is_optional);
+    let quote_methods = quote_methods(field_name, method_name, is_vec);
 
     Ok((quote_field, quote_methods))
 }
--- packages/derive_builder/src/quote_methods.rs
+++ packages/derive_builder/src/quote_methods.rs
@@ -1,7 +1,28 @@
 use proc_macro2::{ TokenStream, Ident };
 use quote::quote;
 
-pub fn quote_all_at_once_setter_method(field_name: &Ident, type_name: TokenStream) -> TokenStream {
+pub fn quote_methods(field_name: &Ident, method_name: Option<&Ident>, is_vec: bool) -> TokenStream {
+    if let Some(method_name) = method_name {
+        let conflicting_names = field_name == method_name;
+
+        if conflicting_names {
+            quote_global_method(field_name, method_name)
+        } else {
+            quote_both_methods(field_name, method_name)
+        }
+    } else {
+        let type_name = if is_vec {
+            quote! { Vec<String> }
+        } else {
+            quote! { String }
+        };
+
+        quote_individual_method(field_name, type_name)
+    }
+}
+
+#[inline]
+fn quote_individual_method(field_name: &Ident, type_name: TokenStream) -> TokenStream {
     quote! {
         fn #field_name(&mut self, #field_name: #type_name) -> &mut Self {
             self.#field_name = Some(#field_name);
@@ -10,7 +31,8 @@ pub fn quote_all_at_once_setter_method(field_name: &Ident, type_name: TokenStrea
     }
 }
 
-pub fn quote_one_at_a_time_setter_method(field_name: &Ident, method_name: &Ident) -> TokenStream {
+#[inline]
+fn quote_global_method(field_name: &Ident, method_name: &Ident) -> TokenStream {
     quote! {
         fn #method_name(&mut self, #field_name: String) -> &mut Self {
             if let Some(ref mut field_name) = self.#field_name {
@@ -25,11 +47,12 @@ pub fn quote_one_at_a_time_setter_method(field_name: &Ident, method_name: &Ident
 }
 
 // args and arg, but not conflicting env and env
-pub fn quote_both_setter_methods(field_name: &Ident, method_name: &Ident) -> TokenStream {
-    let quote_global_method = quote_all_at_once_setter_method(field_name, quote! { Vec<String> });
-    let quote_individual_method: TokenStream = quote_one_at_a_time_setter_method(
+#[inline]
+fn quote_both_methods(field_name: &Ident, method_name: &Ident) -> TokenStream {
+    let quote_global_method = quote_global_method(field_name, method_name);
+    let quote_individual_method: TokenStream = quote_individual_method(
         field_name,
-        method_name
+        quote! { Vec<String> }
     );
 
     quote! {
