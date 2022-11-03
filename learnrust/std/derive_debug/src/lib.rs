fb8a0b6d238865703304b43d58416ce6a64cac74
Jason Lenoble <20752619+jlenoble@users.noreply.github.com>
2022-11-03 23:54:41 +0100

    ✨ feat(derive_debug): step 3

--- learnrust/std/derive_debug/src/lib.rs
+++ learnrust/std/derive_debug/src/lib.rs
@@ -3,7 +3,13 @@ use proc_macro2::{ TokenStream, Ident };
 use quote::quote;
 use syn::{ DeriveInput, Error, parse_macro_input, Result };
 
-#[proc_macro_derive(CustomDebug)]
+use synex::Extract;
+
+use crate::quote_chained_calls::quote_chained_calls;
+
+mod quote_chained_calls;
+
+#[proc_macro_derive(CustomDebug, attributes(debug))]
 pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     quote_all(&parse_macro_input!(input as DeriveInput)).into()
 }
@@ -20,13 +26,14 @@ fn quote_impl_debug(input: &DeriveInput) -> Result<TokenStream> {
     let struct_name: &Ident = &input.ident;
     let struct_name_as_string = struct_name.to_string();
 
+    let quote_chained_calls = quote_chained_calls(input.extract()?)?;
+
     Ok(
         quote! {
             impl ::std::fmt::Debug for #struct_name {
                 fn fmt(&self, fmt: &mut ::std::fmt::Formatter<'_>) -> ::std::fmt::Result {
                     fmt.debug_struct(#struct_name_as_string)
-                        .field("name", &self.name)
-                        .field("bitmask", &self.bitmask)
+                        #quote_chained_calls
                         .finish()
                 }
             }
---
+++ learnrust/std/derive_debug/src/quote_chained_calls.rs
@@ -0,0 +1,46 @@
+use proc_macro2::{ TokenStream, Ident };
+use quote::quote;
+use syn::{ Error, Field, Fields, Result, MetaNameValue };
+
+use synex::{ Extract, ExtractIter, ExtractValue };
+
+pub fn quote_chained_calls(fields: &Fields) -> Result<TokenStream> {
+    let mut quote_each_call: Vec<TokenStream> = Vec::new();
+
+    for field in fields.extract_iter()? {
+        quote_each_call.push(quote_chained_call(field)?);
+    }
+
+    Ok(quote! { #(#quote_each_call)* })
+}
+
+fn quote_chained_call(field: &Field) -> Result<TokenStream> {
+    let field_name: &Ident = field.extract()?;
+    let field_name_as_string = field_name.to_string();
+
+    let meta = field.extract_iter()?.next();
+
+    if meta.is_none() {
+        Ok(quote! {
+            .field(#field_name_as_string, &self.#field_name)
+        })
+    } else {
+        let meta = meta.unwrap();
+
+        let MetaNameValue { path, lit, eq_token: _ } = meta.extract()?;
+
+        let name: &Ident = path.extract()?;
+
+        if name != "debug" {
+            return Err(Error::new_spanned(meta, "expected `debug = \"...\"`"));
+        }
+
+        let custom_format = lit.extract_value()?;
+
+        Ok(
+            quote! {
+                .field(#field_name_as_string, &::std::format_args!(#custom_format, &self.#field_name))
+            }
+        )
+    }
+}
