57eb880aab4eaa116c6b939c1cedc06272547b60
Jason Lenoble <20752619+jlenoble@users.noreply.github.com>
2022-11-04 03:26:14 +0100

    ✨ feat(derive_debug): step 4

--- learnrust/std/derive_debug/src/lib.rs
+++ learnrust/std/derive_debug/src/lib.rs
@@ -3,7 +3,7 @@ use proc_macro2::{ TokenStream, Ident };
 use quote::quote;
 use syn::{ DeriveInput, Error, parse_macro_input, Result };
 
-use synex::Extract;
+use synex::{ Extract, define_add_trait_bounds };
 
 use crate::quote_chained_calls::quote_chained_calls;
 
@@ -26,11 +26,14 @@ fn quote_impl_debug(input: &DeriveInput) -> Result<TokenStream> {
     let struct_name: &Ident = &input.ident;
     let struct_name_as_string = struct_name.to_string();
 
+    let generics = add_trait_bounds(input.generics.clone());
+    let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
+
     let quote_chained_calls = quote_chained_calls(input.extract()?)?;
 
     Ok(
         quote! {
-            impl ::std::fmt::Debug for #struct_name {
+            impl #impl_generics ::std::fmt::Debug for #struct_name #ty_generics #where_clause {
                 fn fmt(&self, fmt: &mut ::std::fmt::Formatter<'_>) -> ::std::fmt::Result {
                     fmt.debug_struct(#struct_name_as_string)
                         #quote_chained_calls
@@ -40,3 +43,5 @@ fn quote_impl_debug(input: &DeriveInput) -> Result<TokenStream> {
         }
     )
 }
+
+define_add_trait_bounds!(::std::fmt::Debug);
