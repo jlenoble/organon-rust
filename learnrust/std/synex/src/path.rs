e465e12bd5d203c522d79b609a1cc297e9f163ec
Jason Lenoble <20752619+jlenoble@users.noreply.github.com>
2022-11-05 19:24:35 +0100

    ✨ feat(synex): genericargument extractor
    Extract T,... from  path<T,...>.

--- learnrust/std/synex/src/path.rs
+++ learnrust/std/synex/src/path.rs
@@ -1,5 +1,15 @@
 use proc_macro2::Ident;
-use syn::{ Error, Path, PathSegment, Result, punctuated::Punctuated, token::Colon2 };
+use syn::{
+    AngleBracketedGenericArguments,
+    Error,
+    GenericArgument,
+    Path,
+    PathSegment,
+    Result,
+    punctuated::Punctuated,
+    token::{ Colon2, Comma },
+    PathArguments,
+};
 
 use crate::Extract;
 
@@ -26,5 +36,39 @@ impl Extract<Ident> for Path {
     fn extract(&self) -> Result<&Ident> {
         let &PathSegment { ref ident, arguments: _ } = self.extract()?;
         Ok(ident)
+    }
+}
+
+impl Extract<Punctuated<GenericArgument, Comma>> for Path {
+    fn extract(&self) -> Result<&Punctuated<GenericArgument, Comma>> {
+        if
+            let &PathSegment {
+                ident: _,
+                arguments: PathArguments::AngleBracketed(
+                    AngleBracketedGenericArguments {
+                        colon2_token: _,
+                        lt_token: _,
+                        ref args,
+                        gt_token: _,
+                    },
+                ),
+            } = self.extract()?
+        {
+            Ok(args)
+        } else {
+            Err(Error::new_spanned(self, "expected generic arguments"))
+        }
+    }
+}
+
+impl Extract<GenericArgument> for Path {
+    fn extract(&self) -> Result<&GenericArgument> {
+        let arguments: &Punctuated<GenericArgument, Comma> = self.extract()?;
+
+        if arguments.len() == 1 {
+            Ok(arguments.first().unwrap())
+        } else {
+            Err(Error::new_spanned(self, "expected only one GenericArgument for Path"))
+        }
     }
 }
