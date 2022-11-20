d0db2c253e3d68cebcc03893484ed006189bd687
Jason Lenoble <20752619+jlenoble@users.noreply.github.com>
2022-11-20 18:57:20 +0100

    ♻️ refactor(task): move enums to separate dir

---
+++ learnrust/std/task/src/enums/mod.rs
@@ -0,0 +1,7 @@
+mod mask;
+mod recur;
+mod status;
+
+pub use self::mask::Mask;
+pub use self::recur::Recur;
+pub use self::status::Status;
--- learnrust/std/task/src/lib.rs
+++ learnrust/std/task/src/lib.rs
@@ -1,15 +1,11 @@
 mod entry;
+mod enums;
 mod errors;
-mod mask;
 mod parse;
-mod recur;
-mod status;
 mod task;
 
 pub use crate::entry::Entry;
+pub use crate::enums::*;
 pub use crate::errors::*;
-pub use crate::mask::Mask;
 pub use crate::parse::parse_task_data_file;
-pub use crate::recur::Recur;
-pub use crate::status::Status;
 pub use crate::task::Task;
